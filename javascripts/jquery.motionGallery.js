340467f697f1cce5cb58e9b371047c4a4028582a
Dan <daniel.weaver@gmail.com>
2009-05-13 00:21:39 -0600

    almost there

--- javascripts/jquery.motionGallery.js
+++ javascripts/jquery.motionGallery.js
@@ -4,9 +4,9 @@ jQuery.fn.motionGallery = function(options) {
     var options = jQuery.extend({
         restarea: 6,
         //Set width of the "neutral" area in the center of the gallery.
-        maxspeed: 17,
+        maxspeed: 7,
         //Set top scroll speed in pixels. Script auto creates a range from 0 to top speed.
-        maxwidth: "1000px",
+        maxwidth: 1000,
         //Set to maximum width for gallery - must be less than the actual length of the image train.
         startpos: 0,
         //Set to 1 for left start, 0 for right, 2 for center.
@@ -17,7 +17,7 @@ jQuery.fn.motionGallery = function(options) {
 
 
     return this.each(function() {
-        // For IE - Do not edit
+
         var iedom = document.all || document.getElementById,
         scrollspeed = 0,
         movestate = '',
@@ -32,10 +32,10 @@ jQuery.fn.motionGallery = function(options) {
         function ietruebody() {
             return (document.compatMode && document.compatMode != "BackCompat") ? document.documentElement: document.body;
         };
-        // End for IE
+
         function creatediv() {
-            statusdiv = document.createElement("div");
-            statusdiv.setAttribute("id", "statusdiv");
+            statusdiv = jQuery(this).append('<div></div>');
+            jQuery(statusdiv).attr("id", "statusdiv");
             document.body.appendChild(statusdiv);
             statusdiv = document.getElementById("statusdiv");
             statusdiv.innerHTML = options.endofgallerymsg;
@@ -57,49 +57,52 @@ jQuery.fn.motionGallery = function(options) {
         };
 
         function getposOffset(what, offsettype) {
-            var totaloffset = (offsettype == "left") ? what.offsetLeft: what.offsetTop;
-            var parentEl = what.offsetParent;
-            while (parentEl != null) {
-                totaloffset = (offsettype == "left") ? totaloffset + parentEl.offsetLeft: totaloffset + parentEl.offsetTop;
-                parentEl = parentEl.offsetParent;
-            }
+            var totaloffset = (offsettype == "left") ? what.offset().left : what.offset().top;
+            var parentEl = what.offsetParent();
+            // while (parentEl != null) {
+                totaloffset = (offsettype == "left") ? totaloffset + parentEl.offset().left : totaloffset + parentEl.offset().top;
+                parentEl = parentEl.offsetParent();
+            // }
             return totaloffset;
         };
 
         function moveleft() {
-            if (loadedyes) {
+            // if (loadedyes) {
                 movestate = "left";
-                if (iedom && parseInt(cross_scroll.style.left) > (menuwidth - actualwidth)) {
-                    cross_scroll.style.left = parseInt(cross_scroll.style.left) - scrollspeed + "px";
-                    showhidediv("hidden");
+                if (iedom && parseInt(cross_scroll.css("left")) > (menuwidth - actualwidth)) {
+                    cross_scroll.css({"left" : parseInt(cross_scroll.css("left")) - scrollspeed + "px"});
+                    // showhidediv("hidden");
                 }
-                else
-                showhidediv("visible");
-            }
+                // else
+
+                // showhidediv("visible");
+            // }
             lefttime = setTimeout("moveleft()", 10);
         };
 
         function moveright() {
-            if (loadedyes) {
+            // if (loadedyes) {
                 movestate = "right";
-                if (iedom && parseInt(cross_scroll.style.left) < 0) {
-                    cross_scroll.style.left = parseInt(cross_scroll.style.left) + scrollspeed + "px";
-                    showhidediv("hidden");
+                if (iedom && parseInt(cross_scroll.css("left")) < 0) {
+                    cross_scroll.css({"left" : parseInt(cross_scroll.css("left")) + scrollspeed + "px"});
+                    // showhidediv("hidden");
                 }
-                else
-                showhidediv("visible");
-            }
+                // else
+                // showhidediv("visible");
+            // }
             righttime = setTimeout("moveright()", 10);
         };
 
         // Main function called on mouseover
+
         function motionengine(e) {
             var mainobjoffset = getposOffset(crossmain, "left");
-            var dsocx = (window.pageXOffset) ? pageXOffset: ietruebody().scrollLeft;
-            var dsocy = (window.pageYOffset) ? pageYOffset: ietruebody().scrollTop;
-            var curposy = (window.event) ? event.clientX: ((e.clientX) ? e.clientX: "");
-
+            var dsocx = jQuery(window).scrollLeft();
+            var dsocy = jQuery(window).scrollTop();
+            var curposy = (window.event) ? event.clientX : ((e.clientX) ? e.clientX: "");
+	
             curposy -= mainobjoffset - dsocx;
+
 
             var leftbound = (menuwidth - options.restarea) / 2;
             var rightbound = (menuwidth + options.restarea) / 2;
@@ -128,63 +131,78 @@ jQuery.fn.motionGallery = function(options) {
 
         function stopmotion(e) {
             if (!window.opera || (window.opera && e.relatedTarget !== null))
-            if ((window.event && !crossmain.contains(event.toElement)) || (e && e.currentTarget && e.currentTarget != e.relatedTarget && !contains_ns6(e.currentTarget, e.relatedTarget))) {
+
+            // if ((window.event && !crossmain.contains(event.toElement)) || (e && e.currentTarget && e.currentTarget != e.relatedTarget && !contains_ns6(e.currentTarget, e.relatedTarget))) 
+						{
                 clearTimeout(lefttime);
                 clearTimeout(righttime);
                 movestate = "";
             }
         };
 
-        $(document).ready(function() {
-            if (iedom) {
-                // Set crossmain to the element
-                crossmain = jQuery(this);
-                // set the maxwidth of the element to the default at the top if it's currently not set
-                // if (typeof crossmain.style.(options.maxwidth) !== 'undefined')
-                crossmain.css('max-width', options.maxwidth);
-                // Set the menu width.  This is currently returning the width of the element
-                menuwidth = crossmain.offsetWidth;
 
-                // set cross_scroll to the motiongallery element
-                cross_scroll = jQuery("#motiongallery");
+        if (iedom) {
+            // Set crossmain to the element
+            crossmain = jQuery(this);
 
-                // get the actual width of the trueContainer
-                // because we're not wrapping the images, the actual width will be the sum of all the image width, give or take
-                actualwidth = jQuery("#trueContainer").offsetWidth;
+            // set the maxwidth of the element to the default at the top if it's currently not set
+            // if (typeof crossmain.style.(options.maxwidth) !== 'undefined')
+            crossmain.css({'max-width': options.maxwidth + 'px'});
+            // Set the menu width.  This is currently returning the width of the element
+            menuwidth = crossmain.width();
 
-                // move the left style of the motiongallery element
-                if (options.startpos)
-                cross_scroll.style.left = (menuwidth - actualwidth) / options.startpos + 'px';
+            // set cross_scroll to the motiongallery element
+            cross_scroll =  jQuery("#motiongallery");
 
-                crossmain.onmousemove = function(e) {
-                    motionengine(e);
-                };
+            // get the actual width of the trueContainer
+            // because we're not wrapping the images, the actual width will be the sum of all the image width, give or take
+            actualwidth = jQuery("#trueContainer").width();
 
-                crossmain.onmouseout = function(e) {
-                    stopmotion(e);
-                    showhidediv("hidden");
-                };
-            }
+            // move the left style of the motiongallery element
+            if (options.startpos)
 
-            loadedyes = 1
+            cross_scroll.css({
+                'left': (menuwidth - actualwidth) / options.startpos + 'px'
+            });
 
-            if (options.endofgallerymsg != "") {
-                creatediv();
-                positiondiv();
-            }
-            if (document.body.filters) {
-                onresize();
-            }
-        });
+            crossmain.hover(function(e) {
+                motionengine(e);
+            },
+            function(e) {
+                stopmotion(e);
+                // showhidediv("hidden");
+            });
+
+        }
+
+        // loadedyes = 1
+        // 
+        // if (options.endofgallerymsg != "") {
+        //     creatediv();
+        //     positiondiv();
+        // }
+        if (document.body.filters) {
+            onresize();
+        }
 
         onresize = function() {
-            if (typeof jQuery(this) !== 'undefined' && jQuery(this).filters) {
-                jQuery(this).style.width = "0";
-                jQuery(this).style.width = "";
-                jQuery(this).style.width = Math.min(jQuery(this).offsetWidth, options.maxwidth) + 'px';
+            if (typeof crossmain !== 'undefined' && crossmain.filters) {
+                jQuery(this).css({
+                    'width': '0px'
+                });
+                jQuery(this).css({
+                    'width': ''
+                });
+                var math = Math.min(crossmain.offsetWidth, options.maxwidth);
+                jQuery(this).css({
+                    'width': math + 'px'
+                });
             };
-            menuwidth = crossmain.offsetWidth;
-            cross_scroll.style.left = options.startpos ? (menuwidth - actualwidth) / options.startpos + 'px': 0;
+            menuwidth = crossmain.width();
+						var cross_scroll_reset = options.startpos ? (menuwidth - actualwidth) / options.startpos : 0;
+            cross_scroll.css({
+                'left': cross_scroll_reset + "px"
+            });
         };
 
     });
